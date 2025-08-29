c2d226355b544136000a6dc4200ea96c2432ce1a
MPapadak <malvina.papadaki@openbet.com>
2025-08-29 10:57:30 +0300

    alter authors function for translator logic

--- db_files/_function_getFilteredAuthors.sql
+++ db_files/_function_getFilteredAuthors.sql
@@ -10,18 +10,30 @@ RETURNS TABLE (
     "Country" varchar,
     "Continent" varchar,
     created_at TIMESTAMP,
-    "#Books" BIGINT
+    "isAuthor" boolean,
+    "isTranslator" boolean,
+    "#Books" BIGINT,
+    "#Translations" BIGINT
 )
 AS $$
 BEGIN
     RETURN QUERY
+    WITH authors_translations AS (
+        SELECT "TranslatorId", COUNT(DISTINCT b."ID") as "#Translations"
+        FROM "Book" b
+        GROUP by "TranslatorId"
+    )
+
     SELECT 
         a."ID", 
         a."Name", 
         c."Name" AS "Country", 
         con."Name" AS "Continent", 
         date_trunc('minute', a.created_at) as "created_at",
-        COUNT(DISTINCT ba."BookId") AS "#Books"
+        exists(select 1 from "BookAuthor" where "AuthorId"=a."ID") as "isAuthor",
+        exists(select 1 from "Book" b where b."TranslatorId"=a."ID") as "isTranslator",
+        COUNT(DISTINCT ba."BookId") AS "#Books",
+        at."#Translations"
     FROM "Author" a
     LEFT JOIN "Country" c ON c."ID" = a."CountryId"
     LEFT JOIN "Continent" con on con."ID"=c."ContinentId"
@@ -29,12 +41,13 @@ BEGIN
     LEFT JOIN "Book" b ON b."ID" = ba."BookId"
     LEFT JOIN "Type" t ON b."TypeId" = t."ID"
     LEFT JOIN "LibraryLocation" ll ON b."LibraryLocationId" = ll."ID"
+    LEFT JOIN authors_translations at on at."TranslatorId"=a."ID"
     WHERE 
         (p_library IS NULL OR ll."Name" = p_library)
         AND (p_type IS NULL OR t."Name" = ANY(p_type))
         AND (p_country IS NULL OR c."Name" = p_country)
         AND (p_continent IS NULL OR con."Name" = p_continent)
-    GROUP BY a."ID", a."Name", c."Name", con."Name", a.created_at
+    GROUP BY a."ID", a."Name", c."Name", con."Name", a.created_at, "isAuthor", "isTranslator", at."#Translations"
     ORDER BY COUNT(DISTINCT ba."BookId") desc;
 END;
 $$ LANGUAGE plpgsql;
